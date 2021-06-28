b1accc2d6dd1d532dedcdc310d4ed960553e4381
germanoclodi <germano.c.lodi@gmail.com>
2021-06-28 13:13:14 -0300

    include positive field mapping restriction

--- field.py
+++ field.py
@@ -1,12 +1,18 @@
 class Field:
     def __init__(self, x, y):
+        self.x = 0
+        self.y = 0
+        
         try:    
             self.x = int(x)
             self.y = int(y)
             self.error = ""
         except:
             self.field_mapping_error()
+            
         if not isinstance(self.x, int) or not isinstance(self.x, int):
+            self.field_mapping_error()
+        elif self.x <= 0 or self.y <= 0:
             self.field_mapping_error()
         
         self.probes = []
