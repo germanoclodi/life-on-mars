209770221d57412f6ef761f568771993eeefcd71
germanoclodi <germano.c.lodi@gmail.com>
2021-06-28 13:13:40 -0300

    componentize steps

--- life_on_mars.py
+++ life_on_mars.py
@@ -2,31 +2,47 @@ import sys
 from field import Field
 from probe import Probe
 
-with open(sys.argv[1], 'r') as input_file:
-    step = ""
-    for line in input_file:
-        if step == "land":
-            landing_positions = list(filter(lambda x: x != " ", line.split(" ")))
-            landing_positions[-1] = landing_positions[-1].strip()
-            probe = Probe(field)
-            probe.land((landing_positions[0], landing_positions[1]), landing_positions[2])
-            step = "obey"
-        elif step == "obey":
-            if probe.error == "":
-                instructions = list(line.strip())
-                probe.run_instructions(instructions)
-            field.register_probe(probe)
-            step = "land"
-        else:
-            field_matrix = list(filter(lambda x: x != " ", line.split(" ")))
-            field_matrix[-1] = field_matrix[-1].strip()
-            field = Field(field_matrix[0], field_matrix[1])
-            if not field.error == "":
+def run():
+    with open(sys.argv[1], 'r') as input_file:
+        step = ""
+        for line in input_file:
+            if step == "land":
+                probe = land(field, line)
+                step = "obey"
+            elif step == "obey":
+                field = obey(field, probe, line)
+                step = "land"
+            else:
+                field = map_field(line)
+                if not field.error == "":
+                    break
+                step = "land"
+
+            if "str" in line:
                 break
-            step = "land"
 
-        if "str" in line:
-            break
+    for probe in field.probes:
+        print(probe)
 
-for probe in field.probes:
-    print(probe)
+def map_field(line):
+    field_matrix = list(filter(lambda x: x != " ", line.split(" ")))
+    field_matrix[-1] = field_matrix[-1].strip()
+    field = Field(field_matrix[0], field_matrix[1])
+    return field
+
+def land(field, line):
+    landing_positions = list(filter(lambda x: x != " ", line.split(" ")))
+    landing_positions[-1] = landing_positions[-1].strip()
+    probe = Probe(field)
+    probe.land((landing_positions[0], landing_positions[1]), landing_positions[2])
+    return probe
+
+def obey(field, probe, line):
+    if probe.error == "":
+        instructions = list(line.strip())
+        probe.run_instructions(instructions)
+    field.register_probe(probe)
+    return field
+
+if (__name__ == "__main__"):
+    run()
